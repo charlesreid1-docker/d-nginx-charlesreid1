9f444921e3b4fc28b08a73e456ff63198d221c0d
C Reid <charlesreid1@Gmail.com>
2019-03-29 12:14:59 -0700

    update templates and clean apply_templates.py

--- apply_templates.py
+++ apply_templates.py
@@ -1,4 +1,5 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+import os, re, sys
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 """
 Apply Default Values to Jinja Templates
@@ -17,10 +18,48 @@ All configuration files are for charlesreid1.com
 docker pod, nginx, and realted infrastructure.
 """
 
-TEMPLATEDIR = 'conf.d_templates'
-OUTDIR = 'conf.d'
 
-def apply_templates(template_dir, output_dir):
+# Where templates live
+TEMPLATEDIR = 'conf.d_templates'
+
+# Where rendered templates will go
+OUTDIR = 'conf.d_new'
+
+# Should existing files be overwritten
+OVERWRITE = True
+
+# Template variables
+TV = {
+        'server_name_default':  'charlesreid1.com',
+        'server_name_gitea':    'git.charlesreid1.com',
+        'server_name_files':    'files.charlesreid1.com',
+        'server_name_pages':    'pages.charlesreid1.com',
+        'server_name_hooks':    'hooks.charlesreid1.com',
+        'server_name_bots':     'bots.charlesreid1.com',
+
+        'pod_webhooks_server':  'localhost',
+
+        'port_default':         '80',
+        'port_gitea':           '80',
+        'port_files':           '80',
+        'port_pages':           '80',
+        'port_hooks':           '80',
+        'port_bots':            '80',
+
+        'port_ssl_default':     '443',
+        'port_ssl_gitea':       '443',
+        'port_ssl_files':       '443',
+        'port_ssl_pages':       '443',
+        'port_ssl_hooks':       '443',
+        'port_ssl_bots':        '443',
+}
+
+
+
+def apply_templates(template_dir, output_dir, template_vars, overwrite=False):
+    """Apply the template variables to the template files
+    to create rendered nginx configuration files.
+    """
 
     if not os.path.exists(output_dir):
         msg = "Error: output dir %s does not exist!"%(output_dir)
@@ -31,45 +70,34 @@ def apply_templates(template_dir, output_dir):
         raise Exception(msg)
 
     # Jinja env
-    e = Environment(loader=FileSystemLoader('conf.d_templates/'))
-
-    #################
-    # http template
-    tvars = {
-            'server_name_default':  'charlesreid1.com',
-            'server_name_gitea':    'git.charlesreid1.com',
-            'server_name_files':    'files.charlesreid1.com',
-            'server_name_pages':    'pages.charlesreid1.com',
-            'server_name_hooks':    'hooks.charlesreid1.com',
-            'server_name_bots':     'bots.charlesreid1.com',
-            'port_default':         '80',
-            'port_gitea':           '80',
-            'port_files':           '80',
-            'port_pages':           '80',
-            'port_hooks':           '80',
-            'port_bots':            '80',
-    }
-
-
-
-
-
-
-
-
-    # Set default variable values
-    template_variables = {
-            'default_port' :        '80',
-            'gitea_port' :          '3000',
-    }
+    env = Environment(loader=FileSystemLoader('conf.d_templates/'))
 
     # Render templates
-    base_filename = 'playlists.html'
-    template_filename = base_filename + '.j2'
-    contents = env.get_template(template_filename).render(
-            playlists_items = self.playlists,
-            **template_variables
-    )
-    with open('%s/%s'%(output_dir,base_filename),'w') as f:
-        f.write(contents)
+    render_files = ['http.DOMAIN.conf', 'https.DOMAIN.conf', 'https.DOMAIN.subdomains.conf']
+    template_files = [f+'.j2' for f in render_files]
+
+    render_files = [re.sub('DOMAIN',template_vars['server_name_default'],s) for s in render_files]
+
+    for rfile,tfile in zip(render_files,template_files):
+
+        # Get rendered template content
+        content = env.get_template(tfile).render(**template_vars)
+
+        # Write to file
+        dest = os.path.join(output_dir,rfile)
+        if os.path.exists(dest) and overwrite is False:
+            msg = "Error: template rendering destination %s already exists!"%(dest)
+            raise Exception(msg)
+
+        with open(dest,'w') as f:
+            f.write(content)
+
+    print("Rendered the following templates:%s\nOutput files:%s\n"%(
+            "".join(["\n- "+os.path.join(template_dir,j) for j in template_files]),
+            "".join(["\n- "+os.path.join(output_dir,j) for j in render_files])
+    ))
+
+
+if __name__=="__main__":
+    apply_templates(TEMPLATEDIR,OUTDIR,TV,OVERWRITE)
 
